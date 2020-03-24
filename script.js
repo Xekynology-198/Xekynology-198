9fd150a2460fc7bd7f45d2555d224c7f4538e384
Xekynology-198 <55885888+Xekynology-198@users.noreply.github.com>
2020-03-24 23:56:57 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+var name = "Sekinat";
+console.log("My name is"  + name);
+
+var courses = ["Front-end , Design"];
+console.log(courses);
+
+const data = [];
+for (let i=0; i<200; i++){
+    if (i%2===0){
+        data.push(i);
+    }
+}
+console.log(data);
